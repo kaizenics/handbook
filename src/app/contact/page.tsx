ac6eb91a247d06c3a80441530720e57cf298c3c4
kaizenics <nicosejohnsoriano@gmail.com>
2024-05-20 00:42:44 +0800

    feat: Create Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "~/components/sidebar";
+
+export default function Contact() {
+  return (
+    <div className="flex">
+        <Sidebar />
+      <div className="flex-1 relative">
+        <h1 className="text-black">Contact</h1>
+      </div>
+    </div>
+  );
+}
