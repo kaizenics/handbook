2c300f6dd1c0051c86e3976760fa2336fd111944
kaizenics <nicosejohnsoriano@gmail.com>
2024-05-22 13:50:21 +0800

    refactor: Remove unused image and adjust input width on chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -21,14 +21,6 @@ export default function Chat() {
       <div className="flex">
         <Sidebar />
         <div className="flex-1 relative">
-          {!hasStartedConversation && (
-            <Image
-              src={hcdcStroke}
-              alt="HCDC"
-              className="w-[1855px] h-[10px] absolute top-0 right-0 bottom-0 opacity-15"
-              layout="responsive"
-            />
-          )}
           {!hasStartedConversation ? (
             <>
               <div className="mx-5 my-7">
@@ -81,13 +73,13 @@ export default function Chat() {
                   <div className="relative">
                     <input
                       placeholder="Ask Gab and Ana..."
-                      className="font-inter w-[1000px] h-[56px] outline-none py-4 pl-4 pr-14 text-black bg-gray-200 border border-gray-200 rounded-full placeholder:text-red-700"
+                      className="font-inter w-[1040px] h-[56px] outline-none py-4 pl-4 pr-14 tex4t-black bg-gray-200 border border-gray-200 rounded-full placeholder:text-red-700"
                       type="text"
                     />
                     <button
                       className="absolute font-montserrat bg-red-700 py-3 px-3 rounded-full top-2 right-2 transition duration-300 hover:bg-red-800"
                       onFocus={startConversation}
-                  >
+                    >
                       <FaArrowUp className="w-[15px] h-[15px]" />
                     </button>
                   </div>
@@ -95,7 +87,7 @@ export default function Chat() {
               </div>
             </>
           ) : (
-            <div className="font-inter max-w-screen-lg w-[63%] mx-auto my-7">
+            <div className="font-inter max-w-screen-lg w-[65%] mx-auto my-7">
               <div className="mt-5">
                 <h1 className="mt-4 text-black font-inter text-2xl font-bold tracking-tighter uppercase">
                   HCDC Student Handbook
@@ -109,11 +101,18 @@ export default function Chat() {
               </div>
               <div className="relative flex flex-col space-y-4 mt-5">
                 {/* Render the chat messages here */}
-                <div className="bg-gray-400 p-4 rounded-lg place-self-end">
-                  User: Hello
+                <div className="bg-gray-400 p-4 rounded-lg place-self-end max-w-[800px]">
+                  What is the Prescribed haircut for Male?
                 </div>
-                <div className="bg-red-700 p-4 rounded-lg place-self-start">
-                  AI: Hi there! How can I help you today?
+
+                <div className="bg-red-700 p-4 rounded-lg place-self-start max-w-[800px]">
+                  <h1>
+                    The prescribed haircut for men typically refers to a haircut
+                    that is required or recommended in certain formal,
+                    professional, or institutional settings, such as the
+                    military, law enforcement, certain schools, or corporate
+                    environments.
+                  </h1>
                 </div>
                 {/* Add more chat messages as needed */}
               </div>
@@ -121,7 +120,7 @@ export default function Chat() {
                 <div className="relative">
                   <input
                     placeholder="Ask Gab and Ana..."
-                    className="font-inter w-[1030px] h-[56px] outline-none py-4 pl-4 pr-14 text-black bg-gray-200 border border-gray-200 rounded-full placeholder:text-red-700"
+                    className="font-inter w-[1040px] h-[56px] outline-none py-4 pl-4 pr-14 text-black bg-gray-200 border border-gray-200 rounded-full placeholder:text-red-700"
                     type="text"
                   />
                   <button className="absolute font-montserrat bg-red-700 py-3 px-3 rounded-full top-2 right-2 transition duration-300 hover:bg-red-800">
