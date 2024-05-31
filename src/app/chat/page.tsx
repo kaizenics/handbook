42eafa1630e731562aae93a21ddb42c0e49fdb76
kaizenics <nicosejohnsoriano@gmail.com>
2024-06-01 00:07:53 +0800

    refactor: Update chat page layout for better mobile responsiveness

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Sidebar } from "~/components/sidebar";
+import { SideBar } from "~/components/sidebar";
+import { MobileBar } from "~/components/mobilebar";
 import { VscSparkleFilled } from "react-icons/vsc";
 import { FaSearch } from "react-icons/fa";
 import { FaWandMagicSparkles } from "react-icons/fa6";
@@ -13,32 +14,32 @@ export default function Chat() {
   return (
     <>
       <div className="flex h-screen overflow-hidden">
-        <Sidebar />
+        <SideBar />
         <div className="flex-1 mx-auto flex flex-col">
           <div className="flex-1 overflow-y-hidden">
             <div className="sticky top-0 bg-white">
               <div className="px-5 py-3">
-                <h1 className="text-black text-2xl">Welcome Back,</h1>
-                <p className="text-blue-700 font-semibold text-3xl">User</p>
+                <h1 className="text-black text-lg sm:text-2xl">Welcome Back,</h1>
+                <p className="text-blue-700 font-semibold text-xl sm:text-3xl">User</p>
               </div>
             </div>
 
             <div className="max-w-screen-lg h-full px-md mx-auto">
               <div className="flex relative justify-center items-center">
                 <div className="mt-0">
-                  <h1 className="mt-4 text-black font-montserrat lg:text-4xl md:text-3xl font-bold tracking-tighter uppercase">
+                  <h1 className="mt-4 text-black font-montserrat lg:text-4xl sm:text-3xl text-[6vw] font-bold tracking-tighter uppercase">
                     HCDC Student Handbook
                   </h1>
                   <div className="flex space-x-1 justify-end">
-                    <p className="text-black font-montserrat font-regular lg:text-2xl md:text-2xl">
+                    <p className="text-black font-montserrat font-regular lg:text-2xl sm:text-2xl text-[5vw]">
                       an AI Assistant
                     </p>
-                    <VscSparkleFilled className="text-blue-700 lg:w-[30px] lg:h-[30px] md:w-[30px] md:h-[30px]" />
+                    <VscSparkleFilled className="text-blue-700 sm:w-[30px] sm:h-[30px] w-[25px] h-[25px]" />
                   </div>
                 </div>
               </div>
               <div className="flex justify-center items-center">
-                <div className="font-inter grid lg:grid-cols-4 sm:grid-cols-2 gap-2 justify-center items-center my-6">
+                <div className="font-inter grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 sm:gap-2 gap-1 justify-center items-center my-6">
                   {[
                     "What is the prescribe haircut for males?",
                     "What is CET?",
@@ -47,26 +48,25 @@ export default function Chat() {
                   ].map((text, idx) => (
                     <div
                       key={idx}
-                      className="relative md:w-[180px] h-[120px] bg-red-700 border rounded-md transition-all duration-300 hover:scale-105 cursor-pointer p-4"
+                      className="relative sm:w-[180px] sm:h-[120px] h-[50px] mx-3 sm:mx-0 bg-red-700 border rounded-md transition-all duration-300 hover:sm:scale-105 cursor-pointer p-4"
                     >
-                      <p className="text-balance text-white text-sm">{text}</p>
-                      <FaSearch className="w-5 h-5 absolute bottom-3 right-3 text-white" />
+                      <p className="sm:text-balance line-clamp-1 sm:line-clamp-none text-white text-sm">{text}</p>
+                      <FaSearch className="w-5 h-5 absolute bottom-3 right-3 text-white hidden sm:block" />
                     </div>
                   ))}
                 </div>
               </div>
               <div className="font-inter flex justify-center items-center space-x-2 mt-5">
                 <FaWandMagicSparkles className="text-red-700 w-6 h-6" />
-                <h1 className="font-regular text-red-700 lg:text-lg md:text-xl">
+                <h1 className="font-regular text-red-700 md:text-lg">
                   How can I help you today?
                 </h1>
               </div>
-          </div>
-
+            </div>
           </div>
           <div className="sticky bottom-0 w-full pb-4 bg-white">
             <div className="flex justify-center px-2">
-              <div className="relative w-full max-w-2xl">
+              <div className="relative w-full lg:max-w-3xl md:max-w-lg sm:max-w-md max-w-md">
                 <input
                   placeholder="Ask Gab and Ana..."
                   className="w-full h-[57px] px-4 pr-14 py-2 text-black bg-gray-200 border border-gray-200 rounded-full placeholder:text-red-700 outline-none"
@@ -81,6 +81,7 @@ export default function Chat() {
               </div>
             </div>
           </div>
+          <MobileBar />
         </div>
       </div>
     </>
