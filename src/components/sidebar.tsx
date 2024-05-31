3f18e7f97172ee64f96e1b6a6d9b3602bc3f255e
kaizenics <nicosejohnsoriano@gmail.com>
2024-06-01 00:08:00 +0800

    refactor: Add MobileBar component for better mobile navigation

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -11,7 +11,7 @@ import { MdContactPhone } from "react-icons/md";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { LuExternalLink } from "react-icons/lu";
 
-export const Sidebar = () => {
+export const SideBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleSidebar = () => {
@@ -21,104 +21,108 @@ export const Sidebar = () => {
   const router = useRouter();
 
   return (
-    <div
-      className={`sticky top-0 h-screen bg-red-700 overflow-hidden transition-all duration-300 ${
-        isCollapsed ? "w-[80px]" : "w-[250px]"
-      }`}
-    >
-      <div className="flex items-center justify-between mt-8 mx-6">
-        <div className="flex items-center space-x-2">
-          <span
-            className={`mx-4 text-2xl font-bold text-emerald-500 ${
-              isCollapsed ? "hidden" : "block"
-            }`}
-          >
-            <Image src={hcdcIcon} alt="HCDC" width={200} height={200} />
-          </span>
+    <>
+      <div
+        className={`sticky top-0 h-screen bg-red-700 overflow-hidden transition-all duration-300 ${
+          isCollapsed ? "w-[80px]" : "w-[250px]"
+        } hidden md:block`}
+      >
+        <div className="flex items-center justify-between mt-8 mx-6">
+          <div className="flex items-center space-x-2">
+            <span
+              className={`mx-4 text-2xl font-bold text-emerald-500 ${
+                isCollapsed ? "hidden" : "block"
+              }`}
+            >
+              <Image src={hcdcIcon} alt="HCDC" width={200} height={200} />
+            </span>
+          </div>
+          <button onClick={toggleSidebar} className="text-white">
+            {isCollapsed ? <IoMdMenu size={34} /> : <IoMdClose size={34} />}
+          </button>
         </div>
-        <button onClick={toggleSidebar} className="text-white">
-          {isCollapsed ? <IoMdMenu size={34} /> : <IoMdClose size={34} />}
-        </button>
-      </div>
-      {!isCollapsed && (
-        <div className="flex flex-col items-center mt-8">
-          <div className="mt-4">
-            <h1 className="mt-4 font-inter text-xl font-bold tracking-tighter uppercase">
-              Student Handbook
-            </h1>
-            <div className="flex space-x-1 ml-20">
-              <p className="font-inter font-thin text-sm">an AI Assistant</p>
-              <VscSparkleFilled className="w-[25px] h-[25px]" />
+        {!isCollapsed && (
+          <div className="flex flex-col items-center mt-8">
+            <div className="mt-4">
+              <h1 className="mt-4 font-inter text-xl font-bold tracking-tighter uppercase">
+                Student Handbook
+              </h1>
+              <div className="flex space-x-1 ml-20">
+                <p className="font-inter font-thin text-sm">an AI Assistant</p>
+                <VscSparkleFilled className="w-[25px] h-[25px]" />
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      <nav className="font-inter font-thin flex flex-col my-10">
-        <div
-          onClick={() => router.push("/chat")}
-          className="flex items-center py-3 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
-        >
-          <AiFillHome className="w-8 h-8" />
-          <span
-            className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+        )}
+        <nav className="font-inter font-thin flex flex-col my-10">
+          <div
+            onClick={() => router.push("/chat")}
+            className="flex items-center py-3 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
           >
-            Home
-          </span>
-        </div>
-        <div
-          onClick={() => router.push("/about")}
-          className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
-        >
-          <AiFillInfoCircle className="w-8 h-8" />
-          <span
-            className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            <AiFillHome className="w-8 h-8" />
+            <span
+              className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            >
+              Home
+            </span>
+          </div>
+          <div
+            onClick={() => router.push("/about")}
+            className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
           >
-            About
-          </span>
-        </div>
-        <div
-          onClick={() => router.push("/contact")}
-          className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
-        >
-          <MdContactPhone className="w-8 h-8" />
-          <span
-            className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            <AiFillInfoCircle className="w-8 h-8" />
+            <span
+              className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            >
+              About
+            </span>
+          </div>
+          <div
+            onClick={() => router.push("/contact")}
+            className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
           >
-            Contact
-          </span>
-        </div>
-        <div
-          onClick={() => router.push("/library")}
-          className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
-        >
-          <FaListUl className="w-8 h-8" />
-          <span
-            className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            <MdContactPhone className="w-8 h-8" />
+            <span
+              className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            >
+              Contact
+            </span>
+          </div>
+          <div
+            onClick={() => router.push("/library")}
+            className="flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer"
           >
-            Library
-          </span>
-        </div>
-        <div className={`flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer ${isCollapsed ? "hidden" : "block"}`}>
-          <LuExternalLink className="w-8 h-8" />
-          <span
-            className="text-md text-white"
-          >
-            <a href="https://studentportal.hcdc.edu.ph/login" target="_blank">
-              Student Portal
-            </a>
-          </span>
-        </div>
-        <div className={`flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer ${isCollapsed ? "hidden" : "block"}`}>
-          <LuExternalLink className="w-8 h-8" />
-          <span
-            className="text-md text-white"
-          >
-            <a href="https://www.hcdc.edu.ph/" target="_blank">
-              HCDC Page
-            </a>
-          </span>
-        </div>
-      </nav>
-    </div>
+            <FaListUl className="w-8 h-8" />
+            <span
+              className={`text-md text-white ${isCollapsed ? "hidden" : "block"}`}
+            >
+              Library
+            </span>
+          </div>
+          <div className={`flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer ${isCollapsed ? "hidden" : "block"}`}>
+            <LuExternalLink className="w-8 h-8" />
+            <span
+              className="text-md text-white"
+            >
+              <a href="https://studentportal.hcdc.edu.ph/login" target="_blank">
+                Student Portal
+              </a>
+            </span>
+          </div>
+          <div className={`flex items-center py-4 px-4 space-x-3 mx-2 rounded-md transition duration-300 hover:bg-red-600 cursor-pointer ${isCollapsed ? "hidden" : "block"}`}>
+            <LuExternalLink className="w-8 h-8" />
+            <span
+              className="text-md text-white"
+            >
+              <a href="https://www.hcdc.edu.ph/" target="_blank">
+                HCDC Page
+              </a>
+            </span>
+          </div>
+        </nav>
+        
+      </div>
+     
+    </>
   );
 };
